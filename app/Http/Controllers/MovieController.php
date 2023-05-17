1cca6f0c38a3ad9dfb4c1a6116d7b8135cae6d61
henry5711 <henryrom57@gmail.com>
2023-05-17 12:01:03 -0400

    done

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Movie;
 use App\Http\Requests\StoreMovieRequest;
 use App\Http\Requests\UpdateMovieRequest;
+use App\Http\Resources\MovieResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class MovieController extends Controller
 {
@@ -15,7 +19,26 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            DB::beginTransaction();
+            $Movie = Movie::get();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'data' => [
+                    'code'   => $e->getCode(),
+                    'title'  => 'messages.MovieController.index.index.internal_error',
+                    'errors' => $e->getMessage()
+                ]
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+            return response()->json([
+                "message"       => "movies",
+                "response"      => MovieResource::collection($Movie),
+            ]);
+
     }
 
     /**
@@ -26,7 +49,29 @@ class MovieController extends Controller
      */
     public function store(StoreMovieRequest $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $id =  $this->createMovie($request);
+
+            $response = Movie::where('id', $id)->first();
+
+            DB::commit();
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+            return response()->json([
+                'data' => [
+                    'code'   => $e->getCode(),
+                    'title'  => [__('messages.Movie.store.store.internal_error')],
+                    'errors' => $e->getMessage(),
+                ]
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            "message"       => "Se registro una nuevo Movie",
+            "response"      => MovieResource::make($response),
+        ]);
     }
 
     /**
@@ -35,9 +80,29 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function show(Movie $movie)
+    public function show($id)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $Movie = Movie::where('id', $id)->get();
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'data' => [
+                    'code'   => $e->getCode(),
+                    'title'  => [__('messages.Movie.show.show.internal_error')],
+                    'errors' => $e->getMessage()
+                ]
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            "message"       => "detalle de Moviee",
+            "response"      => MovieResource::make($Movie),
+        ]);
     }
 
     /**
@@ -47,9 +112,32 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateMovieRequest $request, Movie $movie)
+    public function update(UpdateMovieRequest $request,$id)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $Movie = Movie::findOrFail($id);
+            $this->updateMovie($Movie, $request);
+
+            $response = Movie::where('id', $id)->first();
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'data' => [
+                    'code'   => $e->getCode(),
+                    'title'  => [__('messages.Movie.update.update.internal_error')],
+                    'errors' => $e->getMessage(),
+                ]
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            "message"       => "Movie actulizado",
+            "response"      => MovieResource::make($response),
+        ]);
     }
 
     /**
@@ -58,8 +146,61 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Movie $movie)
+    public function destroy($id)
     {
-        //
+        try {
+            DB::beginTransaction();
+
+            $Movie = Movie::where('id', $id)->first();
+            if ($Movie) {
+                $Movie->delete();
+            } else {
+                return response()->json([
+                    "errors" => [
+                        "message"       =>  ["no es posible realizar eliminar este Moviee"],
+                    ]
+                ], 422);
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'data' => [
+                    'code'   => $e->getCode(),
+                    'title'  => [__('messages.Movie.delete.delete.internal_error')],
+                    'errors' => $e->getMessage(),
+                ]
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return response()->json([
+            "message"       => "Movie Eliminado",
+        ]);
+    }
+
+    public function filter(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $Movie = Movie::filtro($request)->get();
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'data' => [
+                    'code'   => $e->getCode(),
+                    'title'  => [__('messages.Movie.filter.filter.internal_error')],
+                    'errors' => $e->getMessage()
+                ]
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+
+            return response()->json([
+                "message"       => "filtro Movie",
+                "response"      => MovieResource::collection($Movie),
+            ]);
+
     }
 }
