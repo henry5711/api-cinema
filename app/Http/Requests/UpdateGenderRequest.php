b78bfe5705ee33e3ee8bbae84526c180343a7d0b
henry5711 <henryrom57@gmail.com>
2023-05-17 21:09:50 -0400

    request

--- app/Http/Requests/UpdateGenderRequest.php
+++ app/Http/Requests/UpdateGenderRequest.php
@@ -13,7 +13,7 @@ class UpdateGenderRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class UpdateGenderRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'                  => 'nullable|string',
+            'description'                 => 'nullable|string',
         ];
     }
 }
--- app/Http/Requests/UpdateMovieRequest.php
+++ app/Http/Requests/UpdateMovieRequest.php
@@ -13,7 +13,7 @@ class UpdateMovieRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class UpdateMovieRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'                  => 'required|string',
+            'description'                 => 'required|string',
+            'duration'                 => 'required|string',
+            'gender_id'            => 'required|integer|exists:genders,id',
         ];
     }
 }
